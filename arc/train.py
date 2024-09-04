3a7620ffecbbfa5afbbd6b6c30fbb9a68127ae0e
Cosmin-George Alexandru <cosmingeorge.alexandru@gmail.com>
2024-09-04 23:01:52 +0300

    Add token class weights and lr scheduler

--- arc/train.py
+++ arc/train.py
@@ -1,10 +1,14 @@
 import datetime
 import os
 import os.path
-import torch.utils.data
+import typing
 
 import lightning
+import lightning.pytorch.callbacks
 import datasets
+import torch.optim
+import torch.utils.data
+import torch.nn
 
 import arc.dataset
 import arc.transformer
@@ -12,11 +16,24 @@ import arc.transform
 
 
 class ARCEncoderLightning(lightning.LightningModule):
-    def __init__(self, encoder: arc.transformer.ARCEncoder, alpha: float = 1.0):
+    def __init__(
+        self,
+        config: arc.transformer.ARCEncoderConfig,
+        num_epochs: int,
+        alpha: float = 1.0,
+        lm_class_weights: typing.Optional[list[float]] = None,
+    ):
         super().__init__()
-        self.encoder = encoder
-        self.alpha = torch.tensor(alpha)
-        self.save_hyperparameters(ignore=["encoder"])
+        self.encoder = arc.transformer.ARCEncoder(config)
+        self.alpha = alpha
+        self.config = config
+        self.lm_loss = torch.nn.CrossEntropyLoss(
+            weight=torch.tensor(lm_class_weights),
+            ignore_index=self.encoder.config.padding_index,
+            reduction="mean",
+        )
+        self.num_epochs = num_epochs
+        self.save_hyperparameters(ignore=["num_epochs"])
 
     def compute_losses(self, batch, batch_index) -> dict[str, torch.Tensor]:
         enable_problem_type_head = self.encoder.config.num_problem_types > 0
@@ -28,11 +45,9 @@ class ARCEncoderLightning(lightning.LightningModule):
         )
         shifted_lm_logits = outputs["lm_logits"][:, 0:-1, :].contiguous()
         shifted_lm_labels = batch["input_ids"][:, 1:].contiguous()
-        lm_loss = torch.nn.functional.cross_entropy(
+        lm_loss = self.lm_loss(
             shifted_lm_logits.view(-1, shifted_lm_logits.shape[-1]),
             shifted_lm_labels.view(-1),
-            ignore_index=self.encoder.config.padding_index,
-            reduction="mean",
         )
 
         problem_type_loss = None
@@ -70,16 +85,19 @@ class ARCEncoderLightning(lightning.LightningModule):
             self.log("test_pt_loss", problem_type_loss)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-5)
-        return optimizer
+        optimizer = torch.optim.AdamW(self.parameters(), lr=1e-4)
+        lr_scheduler = {
+            "scheduler": torch.optim.lr_scheduler.CosineAnnealingLR(
+                optimizer, self.num_epochs, eta_min=0, last_epoch=-1
+            ),
+            "name": "lr",
+        }
+        return [optimizer], [lr_scheduler]
 
 
 def main():
     torch.set_float32_matmul_precision("high")
-    tokenizer_max_run_length = 30
-    tokenizer = arc.tokenizer.ARCTokenizer(
-        max_run_length=tokenizer_max_run_length
-    )
+    tokenizer = arc.tokenizer.ARCTokenizer(max_run_length=30)
     experiments_path = os.path.join(
         "./experiments", datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
     )
@@ -93,7 +111,7 @@ def main():
         num_heads=8,
         dim_feedforward=2048,
         num_layers=4,
-        tokenizer_max_run_length=tokenizer_max_run_length,
+        tokenizer_max_run_length=tokenizer.max_run_length,
     )
     create_checkpoint = lightning.pytorch.callbacks.ModelCheckpoint(
         dirpath=checkpoints_path,
@@ -110,6 +128,19 @@ def main():
     dataset = datasets.load_dataset(
         "./data/augmented/", keep_in_memory=True
     ).with_format("torch")
+    dataset = dataset.map(
+        lambda ex: {
+            "bins": torch.bincount(
+                ex["tokenized_sequence"], minlength=tokenizer.num_token_ids
+            )
+        },
+        num_proc=16,
+    )
+    lm_class_weights = torch.sum(dataset["train"]["bins"], dim=0)
+    lm_class_weights = lm_class_weights.sum() / (
+        tokenizer.num_token_ids * lm_class_weights
+    )
+    print(f"{lm_class_weights=}")
     dataset = dataset.class_encode_column("problem")
     dataset = dataset["train"].train_test_split(
         test_size=0.25,
@@ -137,10 +168,21 @@ def main():
         ),
         pin_memory=True,
     )
-    model = ARCEncoderLightning(arc.transformer.ARCEncoder(config))
+    num_epochs = 10
+    model = ARCEncoderLightning(
+        config,
+        num_epochs=num_epochs,
+        alpha=1.0,
+        lm_class_weights=lm_class_weights.tolist(),
+    )
     trainer = lightning.Trainer(
-        max_epochs=10,
-        callbacks=[create_checkpoint],
+        max_epochs=num_epochs,
+        callbacks=[
+            create_checkpoint,
+            lightning.pytorch.callbacks.LearningRateMonitor(
+                logging_interval="step"
+            ),
+        ],
         default_root_dir=logs_path,
         accumulate_grad_batches=4,
         gradient_clip_val=1.0,
